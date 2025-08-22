78ebf1e2e0b1cecce943cb484b9785a520896ce6
yurii macbook pro <yurikorock777@gmail.com>
2025-08-22 19:30:48 +0300

    add RecipeCard

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,5 +1,21 @@
-export default function RecipeCard(){
-    return(
-        <div>RecipeCard</div>
-    )
-};
+import css from './RecipeCard.module.css';
+
+export default function RecipeCard({
+  thumb,
+  title,
+  time,
+  description,
+  calories,
+}) {
+  return (
+    <div>
+      <img src={thumb} alt={title} />
+      <h3>{title}</h3>
+      <p>{time} min</p>
+      <p>{description}</p>
+      <p>~{calories ?? 'N/A'} cals</p>
+      <button>Learn more</button>
+      <button>S</button>
+    </div>
+  );
+}
--- src/components/RecipesList/RecipesList.jsx
+++ src/components/RecipesList/RecipesList.jsx
@@ -1,12 +1,26 @@
-import LoadMoreBtn from "../LoadMoreBtn/LoadMoreBtn.jsx";
-import RecipeCard from "../RecipeCard/RecipeCard.jsx";
+import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn.jsx';
+import RecipeCard from '../RecipeCard/RecipeCard.jsx';
 
-export default function RecipesList (){
-    return (
-        <div>
-        <div>RecipesList</div>
-        <RecipeCard/>
-        <LoadMoreBtn/>
-        </div>
-    )
-};
+import recipes from './recipes.json';
+
+export default function RecipesList() {
+  return (
+    <div>
+      <ul>
+        {recipes.map(({ _id, thumb, title, time, description, calories }) => (
+          <li key={_id}>
+            <RecipeCard
+              thumb={thumb}
+              title={title}
+              time={time}
+              description={description}
+              calories={calories}
+            />
+          </li>
+        ))}
+      </ul>
+
+      <LoadMoreBtn />
+    </div>
+  );
+}
