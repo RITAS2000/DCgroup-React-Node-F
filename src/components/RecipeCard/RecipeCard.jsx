ee16b0acfaf57bde9e49725dec87be398be7c4b3
yurii macbook pro <yurikorock777@gmail.com>
2025-08-22 22:10:04 +0300

    add styles to recipeCart

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -8,12 +8,34 @@ export default function RecipeCard({
   calories,
 }) {
   return (
-    <div>
-      <img src={thumb} alt={title} />
-      <h3>{title}</h3>
-      <p>{time} min</p>
-      <p>{description}</p>
-      <p>~{calories ?? 'N/A'} cals</p>
+    <div className={css.card}>
+      <img className={css.image} src={thumb} alt={title} />
+      <div className={css.title_container}>
+        <h3 className={css.title}>{title}</h3>
+        <div className={css.time_container}>
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 7.61537V12.5481L15.2885 15.2884M19.125 12C19.125 15.935 15.935 19.125 12 19.125C8.06497 19.125 4.875 15.935 4.875 12C4.875 8.06497 8.06497 4.875 12 4.875C15.935 4.875 19.125 8.06497 19.125 12Z"
+              stroke="black"
+              stroke-width="0.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <p className={css.time}>{time}</p>
+        </div>
+      </div>
+      <div className={css.descr_cont}>
+        <p className={css.description}>{description}</p>
+        <p>~{calories ?? 'N/A'} cals</p>
+      </div>
+
       <button>Learn more</button>
       <button>S</button>
     </div>
