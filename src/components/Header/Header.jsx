77bd681b6d3dea74a2cc018b2d8c8730d136d187
KKonovalova2502 <k.konovalova2502@gmail.com>
2025-08-23 17:43:51 +0300

    add some style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,22 @@
 import AuthNav from '../AuthNav/AuthNav.jsx';
+import Logo from '../Logo/Logo.jsx';
 import Navigation from '../Navigation/Navigation.jsx';
 import UserMenu from '../UserMenu/UserMenu.jsx';
+import css from './Header.module.css';
 
 export default function Header() {
   //   const isLoggedIn = useSelector(selectIsLoggedIn);
 
   return (
-    <header>
-      <a href="./index.html">Tasteorama</a>
-      <div>
+    <header className={css.container}>
+      <Logo />
+      <div className={css.nav}>
         <Navigation />
-        <AuthNav />
+        <UserMenu />
 
         {/* {isLoggedIn ? <UserMenu /> : <AuthNav />} */}
       </div>
+      <div className={css.burger}>svg</div>
     </header>
   );
 }
