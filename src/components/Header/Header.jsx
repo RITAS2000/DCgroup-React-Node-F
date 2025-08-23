6e6e1cb5751a0ef7376e435adee1266db75bb5a9
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-08-23 23:29:24 +0300

    fixlogo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,20 +8,9 @@ export default function Header() {
   //   const isLoggedIn = useSelector(selectIsLoggedIn);
 
   return (
-
-//     <header style={{backgroundColor: 'grey'}}>
-//       <a href="./index.html">
-//         <svg width="24" height="24">
-//           <use href="/sprite/symbol-defs.svg#icon-logo" />
-//         </svg>
-//         Tasteorama
-//       </a>
-//       <div>
-
     <header className={css.container}>
       <Logo />
       <div className={css.nav}>
-
         <Navigation />
         <UserMenu />
 
