63ace02fa1b062ec4c58218f7d1e0f890357d8b1
yurii macbook pro <yurikorock777@gmail.com>
2025-08-23 13:48:18 +0300

    add scroll when push btn LoadMore

--- src/components/RecipesList/RecipesList.jsx
+++ src/components/RecipesList/RecipesList.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn.jsx';
 import RecipeCard from '../RecipeCard/RecipeCard.jsx';
 import css from './RecipesList.module.css';
@@ -7,17 +7,32 @@ import recipes from './recipes.json';
 
 export default function RecipesList() {
   const [card, setCard] = useState(12);
+  const lastCardRef = useRef(null);
+  const scrollAfterLoad = useRef(false);
 
   const handleLoadMore = () => {
+    scrollAfterLoad.current = true;
     setCard((prev) => prev + 12);
+    
   };
+
+  useEffect(() => {
+    if (scrollAfterLoad.current && lastCardRef.current) {
+      lastCardRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+      scrollAfterLoad.current = false;
+    }
+  }, [card]);
+
   return (
     <div className={css.recipe_container}>
       <ul className={css.recipe_list}>
-        {recipes
-          .slice(0, card)
-          .map(({ _id, thumb, title, time, description, calories }) => (
-            <li key={_id}>
+        {recipes.slice(0, card).map(({ _id, thumb, title, time, description, calories }, index) => {
+          const isLastNew = index === card - 12; // перший новий елемент після "Load More"
+          return (
+            <li key={_id} ref={isLastNew ? lastCardRef : null}>
               <RecipeCard
                 thumb={thumb}
                 title={title}
@@ -26,8 +41,10 @@ export default function RecipesList() {
                 calories={calories}
               />
             </li>
-          ))}
+          );
+        })}
       </ul>
+
       {card < recipes.length && <LoadMoreBtn onClick={handleLoadMore} />}
     </div>
   );
