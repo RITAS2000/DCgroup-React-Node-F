ab8cf3aa08a2cf00254c7ca35644925c8bfd4e54
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-08-23 13:13:15 +0300

    esc

--- src/components/TestModal/TestModal.jsx
+++ src/components/TestModal/TestModal.jsx
@@ -2,10 +2,29 @@ import { useSelector, useDispatch } from 'react-redux';
 import { closeModal } from '../../redux/modal/slice.js';
 import { selectIsModalOpen } from '../../redux/modal/selectors.js';
 import css from './TestModal.module.css';
+import { useEffect } from 'react';
 
 export default function TestModal() {
   const isOpen = useSelector(selectIsModalOpen);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!isOpen) return;
+
+    const handleKeyDown = (e) => {
+      if (e.key === 'Escape') {
+        dispatch(closeModal());
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [isOpen, dispatch]);
+
+  if (!isOpen) return null;
 
   if (!isOpen) return null; // якщо модалка закрита — нічого не рендеримо
   return (
