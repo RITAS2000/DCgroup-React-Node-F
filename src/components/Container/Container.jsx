a841e294233b7687a02aff8197eba03a9b45cca5
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-08-23 16:36:53 +0300

    fix

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,19 +1,17 @@
 import css from './Container.module.css';
 
 export default function Container({ children, variant }) {
-  const backgroundClass = variant === 'light' ? css.bgLight : css.bgDark;
+  let backgroundClass = '';
+
+  if (variant === 'white') {
+    backgroundClass = css.bgWhite;
+  } else if (variant === 'brown') {
+    backgroundClass = css.bgBrown;
+  } else if (variant === 'transparent') {
+    backgroundClass = css.bgTransparent;
+  }
+
   return (
     <div className={`${css.container} ${backgroundClass}`}>{children}</div>
   );
 }
-
-// {
-//   /* Приклад використання:
-//     <Container variant="light">
-//   <h1>Світлий фон</h1>
-// </Container>
-
-// <Container variant="dark">
-//   <p>Темний фон</p>
-// </Container> */
-// }
