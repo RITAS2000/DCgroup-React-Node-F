e612a2d1e92912329c021699e0b43abc7655806d
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-08-24 16:21:57 +0300

    fix

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,25 +2,16 @@ import Footer from '../Footer/Footer.jsx';
 import Header from '../Header/Header.jsx';
 import css from './Layout.module.css';
 
-
 import ReModalContainer from '../ReUseModal/ReModalContainer/ReModalContainer.jsx';
 
-
 import Main from '../Main/Main.jsx';
-
 
 export default function Layout({ children }) {
   return (
     <div className={css.page}>
       <Header />
-
-   
       <ReModalContainer />
-      <main className={css.main}>{children}</main>
-
-     
       <Main>{children}</Main>
-
       <Footer />
     </div>
   );
