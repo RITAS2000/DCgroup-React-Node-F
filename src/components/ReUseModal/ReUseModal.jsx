cf09939ea2fd6a0ab143f44056338a12a39b0cf6
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-08-24 17:51:51 +0300

    fix

--- src/components/ReUseModal/ReUseModal.jsx
+++ src/components/ReUseModal/ReUseModal.jsx
@@ -3,11 +3,12 @@ import { useDispatch } from 'react-redux';
 import { closeModal } from '../../redux/modal/slice.js';
 import css from './ReUseModal.module.css';
 import { GrClose } from 'react-icons/gr';
+import { useCallback } from 'react';
 
 export default function ReUseModal({ children }) {
   const dispatch = useDispatch();
 
-  const handleClose = () => dispatch(closeModal());
+  const handleClose = useCallback(() => dispatch(closeModal()), [dispatch]);
 
   useEffect(() => {
     const handleKeyDown = (e) => {
