a6f2140a6a8a0c5e7ee0c44a429842d24b8f86a3
yurii macbook pro <yurikorock777@gmail.com>
2025-08-24 18:53:29 +0300

    add pagination

--- src/components/RecipesList/RecipesList.jsx
+++ src/components/RecipesList/RecipesList.jsx
@@ -9,28 +9,39 @@ axios.defaults.baseURL = 'https://dcgroup-react-node-b.onrender.com/';
 
 export default function RecipesList() {
   const [recipes, setRecipes] = useState([]);
-  const [card, setCard] = useState(12);
+  const [page, setPage] = useState(1);
+  const [hasNextPage, setHasNextPage] = useState(true);
+  const [loading, setLoading] = useState(false);
   const lastCardRef = useRef(null);
   const scrollAfterLoad = useRef(false);
 
+  //отримуємо з бекенда всі картки рецептів
+  const fetchRecipes = async (page) => {
+    try {
+        setLoading(true);
+      const response = await axios.get(
+        `api/recipes/search?page=${page}&perPage=12`,
+      );
+      const data = response.data?.data || {};
+      const recipesArray = data.data || [];
+
+      setRecipes((prev) => [...prev, ...recipesArray]); // додаємо нові до старих
+      setHasNextPage(data.hasNextPage);
+    } catch (error) {
+      console.error('Помилка при завантаженні рецептів:', error);
+    }
+    finally{
+        setLoading(false)};
+  };
+
+  useEffect(() => {
+    fetchRecipes(page);
+  }, [page]);
+
   const handleLoadMore = () => {
     scrollAfterLoad.current = true;
-    setCard((prev) => prev + 12);
+    setPage((prev) => prev + 1);
   };
-  //отримуємо з бекенда всі картки рецептів
-  useEffect(() => {
-    const fetchRecipes = async () => {
-      try {
-        const response = await axios.get('api/recipes/search');
-        const recipesArray = response.data?.data?.data || [];
-        // console.log('довжина', recipesArray);
-        setRecipes(recipesArray);
-      } catch (error) {
-        console.error('Помилка при завантаженні рецептів:', error);
-      }
-    };
-    fetchRecipes();
-  }, []);
 
   useEffect(() => {
     if (scrollAfterLoad.current && lastCardRef.current) {
@@ -40,15 +51,14 @@ export default function RecipesList() {
       });
       scrollAfterLoad.current = false;
     }
-  }, [card]);
+  }, [recipes]);
 
   return (
     <div className={css.recipe_container}>
       <ul className={css.recipe_list}>
-        {recipes
-          .slice(0, card)
-          .map(({ _id, thumb, title, time, description, calory }, index) => {
-            const isLastNew = index === card - 12; // перший новий елемент після "Load More"
+        {recipes.map(
+          ({ _id, thumb, title, time, description, calory }, index) => {
+            const isLastNew = index === recipes.length - 1; // перший новий елемент після "Load More"
             return (
               <li
                 className={css.recipe_item}
@@ -64,10 +74,11 @@ export default function RecipesList() {
                 />
               </li>
             );
-          })}
+          },
+        )}
       </ul>
 
-      {recipes.length > card && <LoadMoreBtn onClick={handleLoadMore} />}
+      {recipes.length > 0 && !loading && hasNextPage && (<LoadMoreBtn onClick={handleLoadMore} />)}
       {/* <LoadMoreBtn onClick={handleLoadMore} /> */}
     </div>
   );
