8e543beadcea4c699095daa26ccdf65ae04a0257
KKonovalova2502 <k.konovalova2502@gmail.com>
2025-08-25 22:53:09 +0300

    add modal burger

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,18 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import AuthNav from '../AuthNav/AuthNav.jsx';
 import Logo from '../Logo/Logo.jsx';
 import Navigation from '../Navigation/Navigation.jsx';
 import UserMenu from '../UserMenu/UserMenu.jsx';
 import css from './Header.module.css';
 import { selectIsLoggedIn } from '../../redux/auth/selectors.js';
+import { selectModal } from '../../redux/modal/selectors.js';
+import { openModal } from '../../redux/modal/slice.js';
+import ModalMobileNav from '../ModalMobileNav/ModalMobileNav.jsx';
 
 export default function Header() {
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const modal = useSelector(selectModal);
+  const dispatch = useDispatch();
 
   return (
     <header className={css.container}>
@@ -16,7 +21,15 @@ export default function Header() {
         <Navigation />
         {isLoggedIn ? <UserMenu /> : <AuthNav />}
       </div>
-      <div className={css.burger}>svg</div>
+
+      <div
+        className={css.burger}
+        onClick={() => dispatch(openModal({ type: 'mobileMenu' }))}
+      >
+        svg
+      </div>
+
+      {modal.isOpen && modal.type === 'mobileMenu' && <ModalMobileNav />}
     </header>
   );
 }
