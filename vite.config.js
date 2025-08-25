05f9c51b96af056cfdf0b760b5315d5a4df935d0
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-08-25 15:31:22 +0300

    perevirka

--- vite.config.js
+++ vite.config.js
@@ -8,15 +8,3 @@ export default defineConfig({
     sourcemap: true,
   },
 });
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react-swc';
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   base: '/DCgroup-React-Node-F/', // <--- Додати цей рядок
-//   plugins: [react()],
-//   build: {
-//     sourcemap: true,
-//   },
-// });
