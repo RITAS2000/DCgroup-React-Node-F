3f7102f5263cb33ebafc6618225d958de4d86ad6
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-08-25 15:14:23 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,3 +8,15 @@ export default defineConfig({
     sourcemap: true,
   },
 });
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/DCgroup-React-Node-F/', // <--- Додати цей рядок
+//   plugins: [react()],
+//   build: {
+//     sourcemap: true,
+//   },
+// });
