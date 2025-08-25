8f8e51897ed4a36ebb9e431d153d9418e1ae8917
Vladislav Bondarenko <bondarenkovladislav2018@gmail.com>
2025-08-25 10:58:21 +0300

    change Container.jsx

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,16 +1,11 @@
 import css from './Container.module.css';
 
 export default function Container({ children, variant }) {
-  let backgroundClass = '';
-  if (variant === 'white') {
-    backgroundClass = css.bgWhite;
-  } else if (variant === 'brown') {
-    backgroundClass = css.bgBrown;
-  } else if (variant === 'transparent') {
-    backgroundClass = css.bgTransparent;
-  }
+  const backgroundClass = variant === 'light' ? css.bgLight : css.bgDark;
   return (
-    <div className={`${css.container} ${backgroundClass}`}>{children}</div>
+    <div className={`${css.container} ${variant ? backgroundClass : ''}`}>
+      {children}
+    </div>
   );
 }
 
