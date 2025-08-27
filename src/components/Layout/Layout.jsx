672a244b70e1c356be86ecd2a41faccdea1ebd05
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-08-27 04:39:44 +0300

    logout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,19 +1,28 @@
 import Footer from '../Footer/Footer.jsx';
 import Header from '../Header/Header.jsx';
 import css from './Layout.module.css';
-import ModalContainer from '../ModalContainer/ModalContainer.jsx'
+import ModalContainer from '../ModalContainer/ModalContainer.jsx';
 import { Suspense, lazy } from 'react';
 import { ClockLoader } from 'react-spinners';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const Outlet = lazy(() => import('../Outlet/Outlet.jsx'));
 
 export default function Layout({ children }) {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate('auth/login'); // редірект на логін
+    }
+  }, [isLoggedIn, navigate]);
   return (
     <div className={css.page}>
       <Header />
 
       <ModalContainer />
-
 
       <div className={css['outlet-container']}>
         <Suspense
