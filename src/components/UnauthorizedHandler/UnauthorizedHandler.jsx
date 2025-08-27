8a42f2390ecdd265649c0f1f3799630eabf55728
Yuliya Lev <yuliya.lev.2000@gmail.com>
2025-08-27 13:27:49 +0300

    fix

--- src/components/UnauthorizedHandler/UnauthorizedHandler.jsx
+++ src/components/UnauthorizedHandler/UnauthorizedHandler.jsx
@@ -7,6 +7,7 @@ import { toast } from 'react-toastify';
 const UnauthorizedHandler = () => {
   const dispatch = useDispatch();
   const hasShownToast = useRef(false); // трекнемо чи показаний тост
+  const hasLoggedOut = useRef(false); // трекнемо чи вже робився logout
 
   useEffect(() => {
     const interceptor = axios.interceptors.response.use(
@@ -17,7 +18,11 @@ const UnauthorizedHandler = () => {
             hasShownToast.current = true;
             toast.info('Сесія закінчилась. Будь ласка, увійдіть знову.');
           }
-          dispatch(logout());
+
+          if (!hasLoggedOut.current) {
+            hasLoggedOut.current = true;
+            dispatch(logout());
+          }
         }
         return Promise.reject(error);
       },
